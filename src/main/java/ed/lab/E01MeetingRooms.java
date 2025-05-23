5cf6629e5ff34e423cde523fe1962fa569138812
marcs <mwschaub@correo.url.edu.gt>
2025-05-23 07:53:56 -0600

    ejercicio 1 commit

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,11 +1,29 @@
 package ed.lab;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class E01MeetingRooms {
 
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        return -1;
-    }
+        if (meetingIntervals == null || meetingIntervals.isEmpty()) {
+            return 0;
+        }
 
+        //ordena los intervalos por hora de inicio
+        meetingIntervals.sort(Comparator.comparingInt(MeetingInterval::startTime));
+
+        //(min-heap)
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        //se liberan las salas por "antiguedad"
+        for (MeetingInterval interval : meetingIntervals) {
+            if (!minHeap.isEmpty() && interval.startTime() >= minHeap.peek()) {
+                minHeap.poll();
+            }
+            minHeap.offer(interval.endTime()); //ocupa la sala
+        }
+
+        return minHeap.size();
+    }
 }
