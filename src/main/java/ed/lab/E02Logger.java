63edf438bf0336399f2f71139b4e144d8a99a3e2
marcs <mwschaub@correo.url.edu.gt>
2025-05-23 08:10:08 -0600

    ejercicio 1 y 2 commit

--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -1,12 +1,19 @@
 package ed.lab;
+import java.util.HashMap;
+import java.util.Map;
 
 public class E02Logger {
+    private final Map<String, Integer> mensajeTimestamp;
 
     public E02Logger() {
+        mensajeTimestamp = new HashMap<>();
     }
 
     public boolean shouldPrintMessage(int timestamp, String message) {
+        if (!mensajeTimestamp.containsKey(message) || timestamp >= mensajeTimestamp.get(message)) {
+        mensajeTimestamp.put(message, timestamp + 10);
+            return true;
+        }
         return false;
     }
-
 }
